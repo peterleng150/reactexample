021d7ad7f534bd6dda264c19fa4dfa6c3e845517
root <root@DESKTOP-M3PPLVF.localdomain>
2019-01-01 20:58:53 -0500

    Updates

---
+++ precache-manifest.bd494a7c70f53008afcea905d743115a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactexample/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "20fcad8919664de0f527",
+    "url": "/reactexample/static/js/runtime~main.20fcad89.js"
+  },
+  {
+    "revision": "18e19c76940cfb32ba86",
+    "url": "/reactexample/static/js/main.18e19c76.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/reactexample/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "18e19c76940cfb32ba86",
+    "url": "/reactexample/static/css/main.80a10c4d.chunk.css"
+  },
+  {
+    "revision": "19e726d87fe128761c98d5083223a9ab",
+    "url": "/reactexample/index.html"
+  }
+];
